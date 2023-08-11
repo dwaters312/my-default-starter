aa0a4743d1de5b0f8950d9b67ab7f39fa41d5393
unknown <dman3125@gmail.com>
2023-08-10 20:40:53 -0400

    Initial Check-In

---
+++ src/About.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>ABOUT US</h2>
+                <p>We are a family owned plumbing company that can assist
+                    with all of your WATER needs. Oh, and our last name just
+                    happens to be Waters. 
+                </p>
+
+                <p>For all your water needs, call the Waters Co. </p>
+            </div>
+        );
+    }
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>HELLO</h2>
+                <p>To our amazing customers:
+
+                    We have seen almost everything possible in 
+                    our 103 years of service. That being said, 
+                    we have no current plans to stop work in the 
+                    office or on job sites. If the need should arise,
+                    we will take immediate action to temporarily suspend 
+                    work. We understand the importance of staying on schedule
+                    for our projects but feel that the health and safety of our 
+                    clients and employees must come first.</p>
+
+            </div>
+        );
+    }
+}
+
+export default Home;
